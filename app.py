efd2cf0196c5f696f086d07128fe19374856547a
John Villalvazo <jvillalvazo2@ucmerced.edu.com>
2024-01-13 00:16:00 -0800

    Clean up of main app.py file. Call definition from database.py to create tables

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask
 from markupsafe import escape #used when allowing input or returning a value to prevent of code injection 
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy.orm import DeclarativeBase
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, String, VARCHAR
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from database import * #importing database.py to cleanly create/alter tables + models
 
 
 # create the app
 app = Flask(__name__)
 
-# configure the SQLite database, relative to the app instance folder
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+#create/alter tables call from database.py
+createDB() 
 
-# initialize the app with the extension
-# db.init_app(app)
 
 
 
