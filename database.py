b4fd88c6ae654e1e0321d1d1fc0ec7c79ea73886
John Villalvazo <jvillalvazo2@ucmerced.edu.com>
2024-01-22 23:07:47 -0800

    Updated column to table 'User'

--- database.py
+++ database.py
@@ -1,18 +1,26 @@
+import os.path
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, VARCHAR
+from flask_login import LoginManager
+
+
 
 
 class Base(DeclarativeBase):
   pass
 
-db = SQLAlchemy(model_class=Base)
+
 
 # create the app
 app = Flask(__name__)
+
+db = SQLAlchemy(model_class=Base)
+
 # configure the SQLite database, relative to the app instance folder
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+
 # initialize the app with the extension
 db.init_app(app)
 
@@ -20,6 +28,7 @@ db.init_app(app)
 class User(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     username: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    password: Mapped[VARCHAR] = mapped_column(VARCHAR)
     email: Mapped[str] = mapped_column(String)
     
 class Sites(db.Model):
@@ -31,4 +40,5 @@ class Sites(db.Model):
     
 def createDB():
   with app.app_context():
-    db.create_all()
+    db.create_all() # does not alter a table that already exists
+    
